45c562ea13b6511a36e883dda86e8dc9a59c8679
breakholex <121162502+breakholex@users.noreply.github.com>
2022-12-21 18:01:57 +0700

    Add files via upload

---
+++ start.cmd
@@ -0,0 +1,20 @@
+@echo off
+cd /d %~dp0
+if exist bin\php\php.exe (
+set PHPRC=""
+set PHP_BINARY=bin\php\php.exe
+) else (
+set PHP_BINARY=php
+)
+if exist PocketMine-MP.phar (
+set POCKETMINE_FILE=PocketMine-MP.phar
+) else (
+if exist src\pocketmine\PocketMine.php (
+set POCKETMINE_FILE=src\pocketmine\PocketMine.php
+) else (
+echo "Couldn't find a valid PocketMine-MP installation"
+pause
+exit 1
+)
+)
+%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
